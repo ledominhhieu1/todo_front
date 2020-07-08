d06c9b6eb9b2f6c0085feb1f892c1ef09df22854
unknown <hieuldm@gmail.com>
2020-07-08 20:06:23 +0700

    fourth commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,15 +4,57 @@ import TodoDate from './TodoDate';
 import ItemList from './ItemList';
 
 
+// function todoList() {
+
+//     axios({
+//         method: 'get',
+//         url: 'http://localhost:5321/task',
+//         headers: {
+//             'Authorization': localStorage.getItem('isAuth')
+//         },
+//     }).then(response => {
+//         return response.data;
+//     }).catch(error => { alert('Fail'); });
+// }
+
+// function loadDataTable()
+// {
+//     const datatable = window.Datatable;
+//     document.getElementById("table").datatable({
+//             "ajax":{
+//                 "type":"GET",
+//                 "dataType":"json",
+//                 "url": 'http://localhost:5321/task',
+//                 "headers": {
+//                     'Authorization': localStorage.getItem('isAuth')
+//                 }
+
+//             },
+//             "columns": [
+//                 {
+//                     "data": "_id", "width": "20%"
+                    
+
+//                 },
+//                 {
+//                     "data": "value","width":"80%"
+//                 }
+//             ]
+//         });
+    
+// }
 class Task extends Component {
     render() {
         return (
-            
-                <AppStateProvider>
-            <TodoDate />
-            <ItemList />
-          </AppStateProvider>
-        
+
+            <AppStateProvider>
+                <TodoDate />
+                <ItemList/>
+                 {/* <table id="table"></table>
+                 {loadDataTable()}
+                </ItemList> */}
+            </AppStateProvider>
+
         );
     }
 }
