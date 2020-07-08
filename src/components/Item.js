922601f387b94015843ff5b94f6f4a8e0583009b
unknown <hieuldm@gmail.com>
2020-07-08 20:04:31 +0700

    fourth commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,31 +1,53 @@
 import React from "react";
 import { useAppReducer } from "../AppContext";
 import styles from "./Item.module.scss";
+import axios from 'axios';
 
 // Individual todo item
 function Item({ item }) {
   const dispatch = useAppReducer();
-  let text = item.text;
-  let paused = item.status === "paused";
-  let completed = item.status === "completed";
+  let text = item.value;
+  // let paused = item.status === "paused";
+  let completed = item.isCompleted === true;
+  let itemKey = item._id;
 
   function deleteItem() {
     dispatch({ type: "DELETE_ITEM", item });
+    axios({
+      method: 'del',
+      url: 'http://localhost:5321/task/' + itemKey, 
+      headers: {
+        'Authorization': localStorage.getItem('isAuth')
+      },
+      data: {
+        isRemoved: true
+      }
+    })
   }
 
-  function pauseItem() {
-    const pausedItem = { ...item, status: "paused" };
-    dispatch({ type: "UPDATE_ITEM", item: pausedItem });
-  }
+  // function pauseItem() {
+  //   const pausedItem = { ...item, status: "paused" };
+  //   dispatch({ type: "UPDATE_ITEM", item: pausedItem });
+  // }
 
-  function resumeItem() {
-    const pendingItem = { ...item, status: "pending" };
-    dispatch({ type: "UPDATE_ITEM", item: pendingItem });
-  }
+  // function resumeItem() {
+  //   const pendingItem = { ...item, status: "pending" };
+  //   dispatch({ type: "UPDATE_ITEM", item: pendingItem });
+  // }
 
   function completeItem() {
-    const completedItem = { ...item, status: "completed" };
-    dispatch({ type: "UPDATE_ITEM", item: completedItem });
+    // const completedItem = { ...item, isCompleted: true };
+    // dispatch({ type: "UPDATE_ITEM", item: completedItem });
+    axios({
+      method: 'patch',
+      url: 'http://localhost:5321/task/' + itemKey, 
+      headers: {
+        'Authorization': localStorage.getItem('isAuth')
+      },
+      data: {
+        isCompleted: true
+      }
+    })
   }
 
   return (
@@ -35,7 +57,7 @@ function Item({ item }) {
         className={`${styles.buttons} ${completed ? styles.completedButtons : ""}`}
       >
         <button className={styles.delete} onClick={deleteItem} tabIndex="0"></button>
-        {!paused && !completed && (
+        {/* {!paused && !completed && (
           <button className={styles.pause} onClick={pauseItem} tabIndex="0"></button>
         )}
         {paused && !completed && (
@@ -44,7 +66,7 @@ function Item({ item }) {
             onClick={resumeItem}
             tabIndex="0"
           ></button>
-        )}
+        )} */}
         {!completed && (
           <button
             className={styles.complete}
